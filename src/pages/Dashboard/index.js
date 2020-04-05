b0035cb4222e0da46b4f248db3756c4527722949
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-05 19:52:20 -0300

    Dashboard: estilização

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
 import api from '~/services/api';
 
-// import { Container } from './styles';
-export default function Dashboard() {
-  api.get('appointments');
+import { Container, Time } from './styles';
 
-  return <h1>Dashboard</h1>;
+export default function Dashboard() {
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+
+        <strong>31 de Maio</strong>
+
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Marcelo Silva</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Marcelo Silva</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Marcelo Silva</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
