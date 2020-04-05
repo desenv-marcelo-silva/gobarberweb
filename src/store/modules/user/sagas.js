c43a68ebdccc5ac038908d50e46037627c39bd1c
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-04-05 12:35:09 -0300

    Atualizando dados do perfil no store e no banco.

--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,24 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+    const profile = {
+      name,
+      email,
+      ...(rest.oldPassword ? rest : {}),
+    };
+    const response = yield call(api.put, 'users', profile);
+    yield put(updateProfileSuccess(response.data));
+    toast.success('Perfil atualizado com sucesso.');
+  } catch (error) {
+    toast.error('Erro ao atualizar o perfil. Verifique suas informações.');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
